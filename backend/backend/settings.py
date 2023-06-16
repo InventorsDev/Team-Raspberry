f3dbf58c5569997ac2661a025c7f86914fb6aa25
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2023-06-16 15:49:34 +0100

    change settings to use different host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,17 +81,13 @@ WSGI_APPLICATION = 'backend.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if DEBUG == 'True':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-elif DEBUG == 'False':
-    DATABASES = {
-        'default': dj_database_url.config()
-    }
+}
 
 
 # Password validation
