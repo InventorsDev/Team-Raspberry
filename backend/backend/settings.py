f3543d72196b9fcfb229e4544b78ef0414a2d527
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2023-06-16 13:18:46 +0100

    debug db connection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,17 +80,17 @@ WSGI_APPLICATION = 'backend.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config()
-    }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 
 # Password validation
