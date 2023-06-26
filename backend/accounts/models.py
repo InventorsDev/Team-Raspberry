8fa2dbda27db52915697dd16c7a4aa7792e06fd0
Ayo <omotolatomi01@gmail.com>
2023-06-26 11:42:10 +0100

    empty main

--- backend/accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-
-class MyUser(AbstractUser):
-    profile_picture = models.ImageField(upload_to='profile_img/',null=True,blank=True)
--- backend/main/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Video
-
-class VideoSerializer(serializers.ModelSerializer):
-    creator = serializers.PrimaryKeyRelatedField(read_only=True)
-    class Meta:
-        model = Video
-        fields = '__all__'
