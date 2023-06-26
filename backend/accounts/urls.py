34a8382011efb4985f83fe61df1fd9101284008b
Ayo <omotolatomi01@gmail.com>
2023-06-26 12:08:53 +0100

    empty branch

--- backend/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('signup/',SignUpView.as_view(),name='signup'),
-    path('login/',LoginView.as_view(),name='login'),
-    path('profile/',ProfileView.as_view(),name='profile'),
-]
