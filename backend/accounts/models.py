7d3ff281011863af393c99e526838a26ec3f5d5f
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2023-07-31 14:52:51 +0100

    Add gender and date of birth to user profile

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,11 +4,23 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 USER_TYPE = (
-    ('student','Student'),
-    ('content_creator','Content Creator')
+    ('student', 'Student'),
+    ('content_creator', 'Content Creator')
 )
 
+GENDER_CHOICE = (
+    ('male', 'Male'),
+    ('female', 'Female'),
+    ('other', 'Other')
+)
+
+
 class MyUser(AbstractUser):
-    profile_picture = models.ImageField(upload_to='profile_img/',null=True,blank=True)
+    profile_picture = models.ImageField(
+        upload_to='profile_img/', null=True, blank=True)
     creator_email_verified = models.BooleanField(default=False)
-    user_type = models.CharField(choices=USER_TYPE,max_length=15,default='student')
+    user_type = models.CharField(
+        choices=USER_TYPE, max_length=15, default='student')
+    gender = models.CharField(
+        max_length=10, choices=GENDER_CHOICE, null=True, blank=True)
+    date_of_birth = models.DateField(editable=True, blank=True, null=True)
