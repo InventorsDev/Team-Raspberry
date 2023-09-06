047eb73f526f5d1bd85f029dc212a4ede086cc2c
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2023-09-06 21:52:55 +0100

    update permission for video upload

--- backend/main/views.py
+++ backend/main/views.py
@@ -12,7 +12,7 @@ from .permissions import *
 class VideoList(generics.ListCreateAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly,]
+    permission_classes = [IsAuthenticatedOrReadOnly,IsContentCreatorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(creator_id=self.request.user)
