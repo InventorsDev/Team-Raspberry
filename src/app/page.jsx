62f90c311f4a9ecdfc97ebdc8c9f734fb5f58990
Ayo <omotolatomi01@gmail.com>
2023-09-08 19:31:14 +0100

    work on bug

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,17 +2,17 @@
 
 import { useRouter } from "next/navigation";
 import Welcome from "./welcome/page";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
+
 export default function Home() {
-  const cokkieToken = Cookies.get('token'); 
+  const cokkieToken = Cookies.get("token");
   console.log(cokkieToken);
-  const router=useRouter()
-  if(cokkieToken===undefined || cokkieToken===null || cokkieToken===''){
-    router.push("/")
+  const router = useRouter();
+  if (cokkieToken === undefined || cokkieToken === null || cokkieToken === "") {
+    router.push("/");
   }
   return (
     <>
-   
       <Welcome />
     </>
   );
